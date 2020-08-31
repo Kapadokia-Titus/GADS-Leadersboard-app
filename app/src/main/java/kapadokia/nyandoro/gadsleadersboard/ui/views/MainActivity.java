426a20347d112c299d7672ab653c2b06ebdbb015
Kapadokia-Titus <ktnyandoch@gmail.com>
2020-09-01 02:21:55 +0300

    successfully added the viewPager

--- app/src/main/java/kapadokia/nyandoro/gadsleadersboard/ui/views/MainActivity.java
+++ app/src/main/java/kapadokia/nyandoro/gadsleadersboard/ui/views/MainActivity.java
@@ -1,8 +1,12 @@
 package kapadokia.nyandoro.gadsleadersboard.ui.views;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
 
 import kapadokia.nyandoro.gadsleadersboard.R;
 
@@ -12,5 +16,47 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Create an instance of the tab layout from the view.
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+// Set the text for each tab.
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.hours_fragment));
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.skilliq_fragment));
+// Set the tabs to fill the entire layout.
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+// Use PagerAdapter to manage page views in fragments.
+
+
+        // Use PagerAdapter to manage page views in fragments.
+// Each page is represented by its own fragment.
+        final ViewPager viewPager = findViewById(R.id.pager);
+        final PagerAdapter adapter = new PagerAdapter
+                (getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+// Setting a listener for clicks.
+
+
+
+        // Setting a listener for clicks.
+        viewPager.addOnPageChangeListener(new
+                TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new
+               TabLayout.OnTabSelectedListener() {
+                   @Override
+                   public void onTabSelected(TabLayout.Tab tab) {
+                       viewPager.setCurrentItem(tab.getPosition());
+                   }
+
+                   @Override
+                   public void onTabUnselected(TabLayout.Tab tab) {
+                   }
+
+                   @Override
+                   public void onTabReselected(TabLayout.Tab tab) {
+                   }
+               });
     }
 }
---
+++ app/src/main/java/kapadokia/nyandoro/gadsleadersboard/ui/views/PagerAdapter.java
@@ -0,0 +1,32 @@
+package kapadokia.nyandoro.gadsleadersboard.ui.views;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class PagerAdapter extends FragmentPagerAdapter {
+
+    int mNumOfTabs;
+    public PagerAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+        this.mNumOfTabs = behavior;
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0: return new HoursFragment();
+            case 1: return new SkillIqFragment();
+            default:
+                return null;
+        }
+
+    }
+
+    @Override
+    public int getCount() {
+        return mNumOfTabs;
+    }
+}
